4957e00d67725990912bcc31777dfc300b67989c
highlander <bithighlander@gmail.com>
2021-10-04 22:01:29 -0600

    clean up

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,28 +1,12 @@
-import { TimeIcon } from '@chakra-ui/icons'
 import { AssetsIcon } from 'components/Icons/Assets'
-import { DashboardIcon } from 'components/Icons/Dashboard'
 import { Layout } from 'components/Layout/Layout'
-import { Asset } from 'pages/Assets/Asset'
-import { AssetRightSidebar } from 'pages/Assets/AssetRightSidebar'
-import { Assets } from 'pages/Assets/Assets'
-import { AssetSidebar } from 'pages/Assets/AssetSidebar'
-import { Dashboard } from 'pages/Dashboard/Dashboard'
-import { DashboardSidebar } from 'pages/Dashboard/DashboardSidebar'
 import { NotFound } from 'pages/NotFound/NotFound'
 import { ShapeShift } from 'pages/ShapeShift/ShapeShift'
-import { TradeHistory } from 'pages/TradeHistory/TradeHistory'
 import { Redirect, Route, Switch } from 'react-router-dom'
 
 import { generateAppRoutes, Route as NestedRoute } from './helpers'
 
 export const routes: Array<NestedRoute> = [
-  // {
-  //   path: '/dashboard',
-  //   label: 'navBar.dashboard',
-  //   icon: <DashboardIcon />,
-  //   main: <Dashboard />,
-  //   rightSidebar: <DashboardSidebar />
-  // },
   {
     path: '/shapeshift',
     label: 'navBar.assets',
@@ -35,28 +19,7 @@ export const routes: Array<NestedRoute> = [
         main: <ShapeShift />
       }
     ]
-  },
-  // {
-  //   path: '/assets',
-  //   label: 'navBar.assets',
-  //   main: <Assets />,
-  //   icon: <AssetsIcon color='inherit' />,
-  //   routes: [
-  //     {
-  //       path: '/:network/:address?',
-  //       label: 'Asset Details',
-  //       main: <Asset />,
-  //       leftSidebar: <AssetSidebar />,
-  //       rightSidebar: <AssetRightSidebar />
-  //     }
-  //   ]
-  // },
-  // {
-  //   path: '/trade-history',
-  //   label: 'navBar.tradeHistory',
-  //   icon: <TimeIcon />,
-  //   main: <TradeHistory />
-  // }
+  }
 ]
 
 const appRoutes = generateAppRoutes(routes)
