59ab8d3bb794888e1f2e107be3c96738dd429c2b
highlander <bithighlander@gmail.com>
2021-10-16 16:47:30 -0600

    begin select modal v2

--- src/context/WalletProvider/AssetSelect/Select.tsx
+++ src/context/WalletProvider/AssetSelect/Select.tsx
@@ -1,21 +1,101 @@
-import {ModalBody, ModalHeader, Stack, Button, Image, useClipboard} from '@chakra-ui/react'
-import { useWallet } from 'context/WalletProvider/WalletProvider'
-import React, {useEffect} from 'react'
+import {
+    ModalBody,
+    ModalHeader,
+    Stack,
+    Button,
+    Image,
+    useClipboard,
+    ModalCloseButton,
+    Input,
+    Box, ModalFooter, ModalContent
+} from '@chakra-ui/react'
+import {useWallet, WalletActions} from 'context/WalletProvider/WalletProvider'
+import React, {useCallback, useEffect, useMemo, useState} from 'react'
+import {Card} from "../../../components/Card";
+import {AssetIcon} from "../../../components/AssetIcon";
+import {SwapCurrency} from "@shapeshiftoss/market-service";
+import {useForm} from "react-hook-form";
+import {useTranslate} from "react-polyglot";
+import {useModal} from "../../ModalProvider/ModalProvider";
+import {filterAssetsBySearchTerm} from "../../../components/Modals/SelectAsset/helpers/filterAssetsBySearchTerm/filterAssetsBySearchTerm";
 
 export const Select = ({ }: any) => {
-  const { state } = useWallet()
-  const { code } = state
+    const { state, dispatch } = useWallet()
+    const { balances, exchangeContext, exchangeInfo, status } = state
+    const [sortedAssets, setSortedAssets] = useState<SwapCurrency[]>([])
+    const [filteredAssets, setFilteredAssets] = useState<SwapCurrency[]>([])
+    const { register, watch } = useForm<{ search: string }>({
+        mode: 'onChange',
+        defaultValues: {
+            search: ''
+        }
+    })
+    const translate = useTranslate()
+    const modal = useModal()
 
-  useEffect(() => {
-    console.log("code: ",code)
-  }, [code])
+    const searchString = watch('search')
+    const searching = useMemo(() => searchString.length > 0, [searchString])
+
+    const onSelectAsset = function(asset:string){
+        console.log("onSelectAsset: ",asset)
+        dispatch({ type: WalletActions.SET_ASSET_CONTEXT, asset })
+        //close modal
+        modal.close('select')
+    }
+
+    const fetchTokens = useCallback(async () => {
+        try {
+            let data:any = {}
+            console.log("FINAL BALANCES: ",balances)
+            data.tokens = balances
+            // const sorted = sortBy(data?.tokens, ['name', 'symbol'])
+            console.log("exchangeInfo.assets: ",exchangeInfo?.assets)
+            const sorted = balances?.filter((entry: { symbol: any }) => exchangeInfo?.assets.indexOf(entry?.symbol) > -1);
+            console.log("sorted: ",exchangeInfo?.assets)
+            setSortedAssets(sorted)
+        } catch (e) {
+            console.warn(e)
+        }
+    }, [])
+
+    useEffect(() => {
+        fetchTokens()
+    }, []) // run only once
+
+    useEffect(() => {
+        setFilteredAssets(
+            searching ? filterAssetsBySearchTerm(searchString, sortedAssets) : sortedAssets
+        )
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [searchString]) // run every time the search is updated
 
   return (
     <>
-      <ModalHeader>Asset Select</ModalHeader>
-      <ModalBody>
-        hi
-      </ModalBody>
+        <ModalHeader textAlign='center'>
+            <h2>Select an  </h2>
+            <small>exchange: {exchangeContext}</small>
+            <small>exchangeInfo: {JSON.stringify(exchangeInfo)}</small>
+            <small>status: {JSON.stringify(status)}</small>
+        </ModalHeader>
+        <ModalCloseButton />
+        <ModalBody alignItems='center' justifyContent='center'>
+            <Card variant='inverted' width='auto' borderRadius='xl'>
+                <Input placeholder="Search name or paste contract" />
+            </Card>
+            <Box flex={1}>
+                {balances?.map((key:any)=>(
+                    <div>
+                        <button onClick={() => onSelectAsset(key.symbol)}>
+                            <AssetIcon src={key?.image} boxSize='24px' mr={4} />
+                            {key.symbol} status: {key.balance}
+                        </button>
+                    </div>
+                ))}
+            </Box>
+        </ModalBody>
+        <ModalFooter flexDir='column'>
+            <small>Manage Token lists</small>
+        </ModalFooter>
     </>
   )
 }
