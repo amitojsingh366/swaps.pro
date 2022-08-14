3f65156e289f972c181ed62664c3f5dba9ac255a
Amitoj Singh <amitojsingh366@gmail.com>
2022-08-14 21:07:24 +0300

    make progress bar chnage to green when the step increments

--- src/components/Trade/TradeStatus/StackedProgressBar.tsx
+++ src/components/Trade/TradeStatus/StackedProgressBar.tsx
@@ -9,21 +9,21 @@ export const StackedProgressBar: FC<{ step: number }> = ({ step = 0 }) => {
             <div className={`progress ${step >= 1 ? step === 1 ? 'progress-striped active' : '' : 'inactive'}`} style={{ width: '100%' }}>
                 { // eslint-disable-next-line jsx-a11y/aria-role
                 }
-                <div role="progressbar" style={{ width: '100%', height: '50px' }} className="progress-bar progress-bar-success">
+                <div role="progressbar" style={{ width: '100%', height: '50px' }} className={`progress-bar ${step > 1 ? 'progress-bar-success' : 'progress-bar-warning'}`}>
                     <Flex alignItems='center' justifyContent='center' height='100%' >Deposit Recieved</Flex>
                 </div>
             </div>
             <div className={`progress ${step >= 2 ? step === 2 ? 'progress-striped active' : '' : 'inactive'}`} style={{ width: '100%' }}>
                 { // eslint-disable-next-line jsx-a11y/aria-role
                 }
-                <div role="progressbar" style={{ width: '100%', height: '50px' }} className="progress-bar progress-bar-warning">
+                <div role="progressbar" style={{ width: '100%', height: '50px' }} className={`progress-bar ${step > 2 ? 'progress-bar-success' : 'progress-bar-warning'}`}>
                     <Flex alignItems='center' justifyContent='center' height='100%' >Awaiting Exchange</Flex>
                 </div>
             </div>
             <div className={`progress ${step >= 3 ? step === 3 ? 'progress-striped active' : '' : 'inactive'}`} style={{ width: '100%' }}>
                 { // eslint-disable-next-line jsx-a11y/aria-role
                 }
-                <div role="progressbar" style={{ width: '100%', height: '50px' }} className="progress-bar progress-bar-info">
+                <div role="progressbar" style={{ width: '100%', height: '50px' }} className={`progress-bar ${step > 3 ? 'progress-bar-success' : 'progress-bar-warning'}`}>
                     <Flex alignItems='center' justifyContent='center' height='100%' >Complete</Flex>
                 </div>
             </div>
